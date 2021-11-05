78c59538d8a7d8c9dcc37cce5bfd70661638cfba
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-11-05 15:47:19 +0100

    adding class for section 9

---
+++ lessons/Section9.cpp
@@ -0,0 +1,9 @@
+//
+// Created by jstol on 05.11.2021.
+//
+
+#include "Section9.h"
+
+void Section9::section9() {
+
+}
---
+++ lessons/Section9.h
@@ -0,0 +1,15 @@
+//
+// Created by jstol on 05.11.2021.
+//
+
+#ifndef CPP20_MASTERCLASS_SECTION9_H
+#define CPP20_MASTERCLASS_SECTION9_H
+
+#include <iostream>
+
+class Section9 {
+   static void section9();
+};
+
+
+#endif //CPP20_MASTERCLASS_SECTION9_H
