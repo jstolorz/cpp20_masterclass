9dde3c670be659e35a03324e8b99b1c02a5be4f0
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-11-14 16:20:16 +0100

    adding reading size of array

--- lessons/Section12.cpp
+++ lessons/Section12.cpp
@@ -3,11 +3,10 @@
 void Section12::section12() {
 
     int numbers[] {1, 2, 4, 5, 1, 8, 2, 3, 6, 1, 4, 2};
-    int collection_size = 12;
     int count{};
 
-    for (int i = 0; i < collection_size; ++i) {
-        for (int j = i+1; j < collection_size; ++j) {
+    for (int i = 0; i < std::size(numbers); ++i) {
+        for (int j = i+1; j < std::size(numbers); ++j) {
             if(numbers[i] == numbers[j] && numbers[i] != -101){
                 numbers[j] = -101;
                 ++count;
@@ -16,8 +15,8 @@ void Section12::section12() {
         std::cout << std::endl;
     }
 
-    std::cout << "The collection contains " << (collection_size - count) << " unique numbers, they are : ";
-    for (int i = 0; i < collection_size; ++i) {
+    std::cout << "The collection contains " << (std::size(numbers) - count) << " unique numbers, they are : ";
+    for (int i = 0; i < sizeof(numbers)/sizeof(numbers[0]); ++i) {
         if(numbers[i] != -101)
         std::cout << numbers[i] << " ";
     }
