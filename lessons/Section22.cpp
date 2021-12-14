3dec5d57aecaf1298a772d16f2bdec01135708a5
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-12-14 16:18:22 +0100

    adding inline and recursive example

--- lessons/Section22.cpp
+++ lessons/Section22.cpp
@@ -1,24 +1,33 @@
 #include "Section22.h"
 
-int add_student(){
+inline int add_student(){
     static unsigned int student_count{0};
     student_count++;
     return student_count;
 }
 
-int odd(){
+inline int odd(){
     static int res{-1};
     res += 2;
     return res;
 }
 
-void print_odds(unsigned int count){
+inline void print_odds(unsigned int count){
     for (int i = 0; i < count; ++i) {
         std::cout << odd() << " ";
     }
 }
 
+unsigned int factorial(unsigned int n){
+    if(n != 0){
+        return n * factorial(n - 1);
+    }
+    return 1;
+}
+
 void Section22::section22() {
 
-    print_odds(5);
+    std::cout <<  factorial(5) << std::endl;
 }
+
+
