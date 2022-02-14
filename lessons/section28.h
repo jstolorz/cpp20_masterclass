7453d1dc3002c5c4391bb924c5f05815a09a0444
Janusz <31537161+jstolorz@users.noreply.github.com>
2022-02-14 16:06:50 +0100

    adding copy constructor

--- lessons/section28.h
+++ lessons/section28.h
@@ -42,13 +42,72 @@ private:
     double height{2};
 };
 
-void print_cylinder(const cylinder& c){
-   std::cout << " cylinder " << c.getHeight() << "\n";
+void print_cylinder(const cylinder &c) {
+    std::cout << " cylinder " << c.getHeight() << "\n";
 }
+
+class person {
+public:
+    person() = default;
+
+    person(const std::string &last_name, const std::string &first_name, int age) : last_name(last_name),
+                                                                                    first_name(first_name), age(&age) {}
+
+    person(const person& other)
+     : person(other.get_last_name(),other.get_first_name(), *(other.get_age()))
+    {}
+
+    ~person(){}
+
+public:
+
+    const std::string &get_last_name() const {
+        return last_name;
+    }
+
+    void set_last_name(const std::string &last_name) {
+        person::last_name = last_name;
+    }
+
+    const std::string &get_first_name() const {
+        return first_name;
+    }
+
+    void set_first_name(const std::string &first_name) {
+        person::first_name = first_name;
+    }
+
+    int *get_age() const {
+        return age;
+    }
+
+    void set_age(int *age) {
+        person::age = age;
+    }
+
+public:
+    void print_info(){
+        std::cout << "Person object at: " << this << "[Last name: " << last_name << ", First name: "
+        << first_name << ", age: " << *age << ", age address: " << age << " ] \n";
+    }
+
+
+private:
+    std::string last_name{};
+    std::string first_name{};
+    int *age;
+};
 
 int section() {
 
-    print_cylinder(44.6);
+
+    person p1("John","Snow",25);
+    p1.print_info();
+
+    person p2(p1);
+
+    p2.print_info();
+
 
     return 0;
 }
